9a71a796e17ce625c64a7bf40da7bb62375efcc6
googlpasha <prostobux@yandex.ru>
2020-01-19 16:27:27 +0300

    New version with new function

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-var vnutren_plan = Number(document.getElementById('vnutren_plan').value);
-var vnutren_otdel = Number(document.getElementById('vnutren_otdel').value);
-var narugn_otdel = Number(document.getElementById('narugn_otdel').value);
-var uteplenie = Number(document.getElementById('uteplenie').value);
-var pol = Number(document.getElementById('pol').value);
-var window1 = Number(document.getElementById('window').value);
 var checkBoxes = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var selArr = [];
+var selobjets = document.getElementsByClassName('selectMenu');
+for (let i = 0; i < selobjets.length; i++) {
+    selArr[i] = Number(selobjets[i].value);
+
+}
 
 function changeSum() {
-    var sum = vnutren_plan + vnutren_otdel + narugn_otdel + uteplenie + pol + window1;
+    var sum = 0;
+
+    if(document.getElementById('1').value == "0"){
+        selArr[2] = Number(document.getElementById('2').value.slice(0,-12));
+    }
+    if(document.getElementById('1').value == "1"){
+        selArr[2] = Number(document.getElementById('2').value.slice(6,-6))-1;
+    }
+    if(document.getElementById('1').value == "2"){
+        selArr[2] = Number(document.getElementById('2').value.slice(12))-2;
+    }
+
+    for (let index = 1; index < selArr.length; index++) {
+        sum += selArr[index];
+        
+    }
     for (let i = 0; i < checkBoxes.length; i++) {
         sum += checkBoxes[i];
         
@@ -16,33 +31,8 @@ function changeSum() {
     document.getElementById('sum').textContent = sum;
 }
 
-function vnutren_plan_func(){
-    vnutren_plan = Number(document.getElementById('vnutren_plan').value);
-    changeSum();
-}
-
-function vnutren_otdel_func(){
-    vnutren_otdel = Number(document.getElementById('vnutren_otdel').value);
-    changeSum();
-}
-
-function narugn_otdel_func(){
-    narugn_otdel = Number(document.getElementById('narugn_otdel').value);
-    changeSum();
-}
-
-function uteplenie_func(){
-    uteplenie = Number(document.getElementById('uteplenie').value);
-    changeSum();
-}
-
-function pol_func(){
-    pol = Number(document.getElementById('pol').value);
-    changeSum();
-}
-
-function window_func(){
-    window1 = Number(document.getElementById('window').value);
+function sel(selobj){
+    selArr[selobj.id] = Number(selobj.value);
     changeSum();
 }
 
@@ -62,5 +52,6 @@ function chb(obj){
         }
 
 }
+
 
 changeSum()
